6e1ac829b5c0be86d1d1a494f9733212647173dc
sera-ch <moonlightocgreview@gmail.com>
2024-11-07 10:40:39 +0700

    Add healthcheck

---
+++ src/main/java/com/sera/todo/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.sera.todo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/health")
+public class HealthCheckController {
+    @GetMapping("/alive")
+    public ResponseEntity healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+}
